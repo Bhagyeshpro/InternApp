0489fa1a89e856103d53d491907ca1bb6db17b8e
BhagyeshPro <bhagyeshpaunikar2001@gmail.com>
2022-02-23 21:44:20 +0530

    Intial Commit Dev

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import {styles} from "./Styles"
+
+const Header = () => {
+  return (
+    <View>
+      <Text style={styles.try}>Header</Text>
+    </View>
+  )
+}
+
+export default Header
---
+++ src/Components/Header/Styles.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    trying: {
+        fontSize: 100,
+        backgroundColor: '#000',
+    },
+});
